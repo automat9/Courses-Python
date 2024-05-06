ec5904358da7d505e26775dddc707e090635dd96
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-05-06 15:17:43 +0100

    Update Notatki.py

--- Notatki.py
+++ Notatki.py
@@ -1,4 +1,9 @@
-integer e.g. 1,3,4,5  = int
-real number e.g. 1.4, 54.22, 9493,34555 = float
-string e.g. "Hello World" = str
-Boolean = True(1)/False(0) = bool
+# integer e.g. 1,3,4,5  = int
+# real number e.g. 1.4, 54.22, 9493,34555 = float
+# string e.g. "Hello World" = str
+# boolean = True(1)/False(0) = bool (upper case mandatory)
+# to find out what type of an expression you're dealing with, use type(), e.g. type(-12), type(3.14), type("hello world")
+# use sys.float_info to find out about the specifics of floats for your runtime environment (smallest/largest number that can be represented with them)
+# to convert an integer into a float, use float(), use type(float(2)) to convert and also check its type
+# type(6/2) = float division, type(6//2) = int division
+# float 3.99 won't be rounded up if converted into an integer, the result will be: 3
