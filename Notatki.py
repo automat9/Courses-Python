2d9fb8fc3d80fd681bb633043f90b0f350a37c5c
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-05-07 08:11:52 +0100

    Update Notatki.py

--- Notatki.py
+++ Notatki.py
@@ -5,5 +5,6 @@
 # to find out what type of an expression you're dealing with, use type(), e.g. type(-12), type(3.14), type("hello world")
 # use sys.float_info to find out about the specifics of floats for your runtime environment (smallest/largest number that can be represented with them)
 # to convert an integer into a float, use float(), use type(float(2)) to convert and also check its type
-# type(6/2) = float division, type(6//2) = int division
+# type(6/2) = float division, type(6//2) = result is rounded down to nearest integer (loss of data)
 # float 3.99 won't be rounded up if converted into an integer, the result will be: 3
+# Variables or constants (numbers) are called operands, and math symbols are called operators
