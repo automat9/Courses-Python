7e9dd2567a41d97e16b86edf70258d18b87ae793
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-05-08 06:54:49 +0100

    Update Notatki.py

--- Notatki.py
+++ Notatki.py
@@ -8,3 +8,5 @@
 # type(6/2) = float division, type(6//2) = result is rounded down to nearest integer (loss of data)
 # float 3.99 won't be rounded up if converted into an integer, the result will be: 3
 # Variables or constants (numbers) are called operands, and math symbols are called operators
+# Tuples are an ordered sequence written as comma-separated elements within parentheses e.g. Ratings = (1,2,3,4,5)
+# Another example: tuple1 = ("disco",10,1.2), if we type tuple1[0]: "disco, duple1[2]: 1.2
