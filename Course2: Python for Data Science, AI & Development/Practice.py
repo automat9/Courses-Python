050c2c996aed536659bed02ee88fc130a0bd90f8
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-05-13 21:17:35 +0100

    Update Practice.py
    Practising things I'm struggling with (hopefully not for long)

--- Course2: Python for Data Science, AI & Development/Practice.py
+++ Course2: Python for Data Science, AI & Development/Practice.py
@@ -1,1 +1,10 @@
-print("hello world")
+#--------------------------IF STATEMENTS--------------------------#
+name = "Mateusz Pawlaczyk" # Assigning name to variable
+if name: # the logic here is that "if name is True" (has a value/isn't empty i.e. name = "")
+    print("My name is:", name)
+# This can be rewritten as:
+name = "Reddosław Czerwiński"
+if name == name: # this is a bit unintuitive but it checks if the value of name is exactly equal to name, which in this case means the same as name == "Reddosław Czerwiński"
+    print(name)
+
+
