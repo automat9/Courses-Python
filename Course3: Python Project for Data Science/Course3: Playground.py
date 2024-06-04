2d594c57d0618e972460c40ad164acf0a4a1f49a
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-06-04 06:38:09 +0100

    Update Course3: Playground.py

--- Course3: Python Project for Data Science/Course3: Playground.py
+++ Course3: Python Project for Data Science/Course3: Playground.py
@@ -1,3 +1,5 @@
+#-------------------------------------------------------------------------------------------------------------
+# Extracting Stok Data using yFinance, a Python Library
 !pip install yfinance==0.2.4
 !pip install pandas==1.3.3
 import yfinance as yf
@@ -36,3 +38,52 @@ apple_share_price_data.plot(x="Date", y="Open")
 
 apple.dividends # company profits returned per share an investor owns, period defined in the "history" function
 apple.dividends.plot() # guess what, this will plot the dividends over time :O
+
+#-------------------------------------------------------------------------------------------------------------
+# Extracting Stock Data using Web Scraping
+# We have to use this as well as not all stock data is available via the API in the assignment, historical data will be extracted using beautiful soup
+!pip install pandas==1.3.3
+!pip install requests==2.26.0
+!mamba install bs4==4.10.0 -y
+!mamba install html5lib==1.1 -y 
+!pip install lxml==4.6.4
+!pip install plotly==5.3.1
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+# To ignore warnings:
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+# Scenario - using yahoo finance website and looking to extract Netflix Data
+# we want to extract: date, open, high, low, close and volume
+
+# Steps for extracting the data
+# 1. Send an HTTP request to the web page using the requests library.
+# 2. Parse the HTML content of the web page using BeautifulSoup.
+# 3. Identify the HTML tags that contain the data you want to extract.
+# 4. Use BeautifulSoup methods to extract the data from the HTML tags.
+# 5. Print the extracted data
+
+# Step 1
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/netflix_data_webpage.html"
+data = requests.get(url).text
+print(data)
+# requests.get() takes a URL as its first argument, this specifies the location of the resoruce, .text is used for extracting the HTML as a string in order to read it
+# the result is a veeeeery long and messy string of text, its difficult to understand and read, so let's parse it :)
+
+# Step 2
+# let's recap: parsing is the process of analysing a string or a data structure to understand its meaning. 
+# it involves breaking down a piece of text or data into its individual componenets or elements, and then analysing them to extract
+# the desired information, or to understand their relationships and meaning
+# after running the cell, you'll find that the result is still quite difficult to read, but it'll be easier to manipulate in Python
+soup = BeautifulSoup(data, 'html5lib')
+# 2 arguments to constractor:
+# a) the html or xml content that you want to parse
+# b) name of the parser that you want to use to parse the content, this is optional, if left blank BeatifulSoup will use the default included in library
+
+# Step 3
+netflix_data = pd.DataFrame(columns=["Date", "Open", "High", "Low", "Close", "Volume"])
+25min
