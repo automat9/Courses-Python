009d022572e05adf1a9c46c893a636371820d736
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-06-07 20:37:04 +0100

    Update Course4: Playground.py

--- Course4: Statistics for Data Science with Python/Course4: Playground.py
+++ Course4: Statistics for Data Science with Python/Course4: Playground.py
@@ -1,1 +1,13 @@
-16min
+# Types of statistical data
+# Cross-sectional = measurement taken at one time period, e.g.students evaluating a course
+# Time series - data collected over time, e.g. uneployment rate
+
+# Number of variables
+# Univariate - data consisting of a single variable to measure some entity
+# Multivariate - data consisting of two or more variables to measure some entity
+
+# Variable types
+# Categorical (nominal) - sorted into mutually exclusive categories, e.g. own or rent a flat, mode of travel, gender, type of employment, NO QUANTITIATIVE RELATIONSHIPS AMONG CATEGORIES AND AVERAGES ARE USUSALLY MEANINGLESS
+# Ordinal - ordered according to some relationship to one another, e.g. number of cars owned by a household, categories can be compared with one another, usually meaningless because no fixed units of measurement, i.e. differences are meaningless
+# Ratio - data with no natural zero, e.g. sales dollars, length, weight, time from start of a process, business data, strongest form of measurement, both ratios and differences are meaningful
+# Interval - ordered and characterised by a specific measure of distance between observations, but with no natural zero, e.g. temperature, time, survey scales, ratios are meaningless (50 degrees is not twice as hot as 25 degrees), differences are meaningful, so averages can be compared
