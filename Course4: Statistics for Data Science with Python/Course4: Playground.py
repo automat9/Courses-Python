c42012c82521f9ba628c835c9e488e111ee1a097
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-06-09 19:25:34 +0100

    Update Course4: Playground.py

--- Course4: Statistics for Data Science with Python/Course4: Playground.py
+++ Course4: Statistics for Data Science with Python/Course4: Playground.py
@@ -1,3 +1,4 @@
+# =================== Intro to Statistics ==========================================================================================================================================================================================================================================================
 # Types of statistical data
 # Cross-sectional = measurement taken at one time period, e.g.students evaluating a course
 # Time series - data collected over time, e.g. uneployment rate
@@ -12,15 +13,84 @@
 # Ratio - data with no natural zero, e.g. sales dollars, length, weight, time from start of a process, business data, strongest form of measurement, both ratios and differences are meaningful
 # Interval - ordered and characterised by a specific measure of distance between observations, but with no natural zero, e.g. temperature, time, survey scales, ratios are meaningless (50 degrees is not twice as hot as 25 degrees), differences are meaningful, so averages can be compared
 
+# 1) use df.info() to find columns and their data types
+# 2) use df.describe() to find: Mean, sd. itq.range and min/max values
+
+# ==================================== #
 # Measures of central tendency
-# ===== Terminology and Notation ===== #
 # x̄ - Sample mean
 # Σ - Operation of addition ("the sum of")
 # N - size of population
 # n - size of sample
-# ====================================
+# ==================================== #
+# Measures of Dispersion
+# Dispersion - degree of variation in the data (is the majority of data in one place, or is it spread out)
+# Low SD =  more consistency, majority of values close to each other, high SD means larger spread from the mean
+# REMEMBER: average = partial picture, avg. statistics are incomplete without SD/var, risk metrics are ALL ABOUT VARIANCE!
+# Range - max - min = range
+# ==================== Let the fun begin :) =========================================================================================================================================================================================================================================================
 
-# 1) use df.info() to find columns and their data types
-# 2) Mean, Median, Mode
+# Import libraries and read the csv file from the URL (copied code from labs)
 
-50min left, measure of central tend: 3rd min
+import piplite
+await piplite.insall(["numpy"],["pandas"])
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as pyplot
+
+from js import fetch
+import io
+
+URL = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ST0151EN-SkillsNetwork/labs/teachingratings.csv'
+resp = await fetch(URL)
+ratings_url = io.BytesIO((await resp.arrayBuffer()).to_py())
+
+ratings_df=pd.read_csv(ratings_url) # now very important, ratings_df will now be used as a variable for all the other commands
+# ==================== Basic Info =====================================================
+ratings_df.head() # shows first five rows of the data
+ratings_df.head(25) # first 25
+ratings_df.trail(25) # last 25
+
+ratings_df.info() # shows info about each variable - (non-null count, data type(obj,int,float, etc))
+
+ratings_df.shape # prints as (number of rows, no. of columns)
+
+# The dataset  is cross-sectional because - no time variable, observes multiple persons and compares them at one point in time
+
+# ==================== Measures of central tendancy (really cool stuff :)) =====================================================
+ratings_df.describe() # describes the whole dataset
+ratings_df['students'].describe() # you're never gonna believe which column this code describes
+
+# ohhh, you're only interested in individual measures, alright then:
+ratings_df['students'].mean()
+ratings_df['students'].median()
+ratings_df['students'].mode()
+ratings_df['students'].min()
+ratings_df['students'].max()
+# happy?
+
+# Now let's create a histogram :D
+pyplot.hist(ratings_df['beauty'])
+# Now let's create a bar chart :D
+pyplot.bar(ratings_df.gender.unique(), ratings_df.gender.value_counts(),color=["pink","blue"])
+pyplot.xlabel('Gender')
+pyplot.ylabel('Count')
+pyplot.title('Gender distribution bar plot')
+
+# create a table to compare genders and their beauty results
+ratings_df.groupby('gender').agg({'beauty':['mean', 'std', 'var']}).reset_index() 
+
+# calculate the percentage of males and females that are tenured professors.
+# first groupby to get the total sum
+tenure_count = ratings_df[ratings_df.tenure == 'yes'].groupby('gender').agg({'tenure': 'count'}).reset_index()
+# find %
+tenure_count['percentage'] = 100 * tenure_count.tenure/tenure_count.tenure.sum()
+tenure_count
+
+# To find median evaluation score for tenured professors (the tricky part is that this is not a numerical value)
+ratings_df[ratings_df['tenure'] == 'yes']['eval'].median()
+# you can index just tenured professors and find their median evaluation scores
+
+# To compare avg age with tenure, to produce the means and sd for both tenured and unt. professors:
+ratings_df.groupby('tenure').agg({'age':['mean', 'std']}).reset_index()
