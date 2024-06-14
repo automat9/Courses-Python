4da54e1ddfefdf4e03074825ba4fc6b8b1bd80ea
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-06-14 08:20:44 +0100

    Update Statistics: Summary.py

--- Course4: Statistics for Data Science with Python/Statistics: Summary.py
+++ Course4: Statistics for Data Science with Python/Statistics: Summary.py
@@ -1,2 +1,9 @@
 # Z-score = (X - μ)/σ - i.e your score - population mean / standard deviation
-35min
+
+# Z-test
+# 1) use when population std is known
+
+# T-test - comparison of average values between 2 groups
+# e.g. testing if teaching evaluations of male instructors are the same for female instructors
+# 1) use when population std is unknown
+# 2) also when comparing the means of 2 independent samples with equal or unequal variances
