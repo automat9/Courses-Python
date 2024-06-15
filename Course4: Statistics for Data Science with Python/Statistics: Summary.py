fddf6fd578d1a93ea517ff73c55856e04fe38b50
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-06-15 22:01:51 +0100

    Update Statistics: Summary.py

--- Course4: Statistics for Data Science with Python/Statistics: Summary.py
+++ Course4: Statistics for Data Science with Python/Statistics: Summary.py
@@ -7,3 +7,9 @@
 # e.g. testing if teaching evaluations of male instructors are the same for female instructors
 # 1) use when population std is unknown
 # 2) also when comparing the means of 2 independent samples with equal or unequal variances
+
+# ANOVA (Analysis of Variance)
+# 1) used if dealing with more than 2 groups - comparing means of more than 2 groups
+# 2) for this, we use F-distribution to compare the mean values for more than 2 groups
+# 3) null = samples in all groups are drawn from the same populations with the same mean values
+# 4) reject null = if p-value or sig for f-test is greater than 0.05
