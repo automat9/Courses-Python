722bdcd9c47838284288e6c7f9b0307de8d26b95
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-06-16 16:48:15 +0100

    Update Statistics: Summary.py

--- Course4: Statistics for Data Science with Python/Statistics: Summary.py
+++ Course4: Statistics for Data Science with Python/Statistics: Summary.py
@@ -11,15 +11,32 @@
 
 # T-test - comparison of average values between 2 groups
 # e.g. testing if teaching evaluations of male instructors are the same for female instructors
-# 1) use when population std is unknown
-# 2) also when comparing the means of 2 independent samples with equal or unequal variances
+# Assumptions:
+# 1) One independent, categorical variable with two levels or group
+# 2) One dependent continous variable
+# 3) Independence of the observations. Each subject should belong to only one group. There is no relationship between the observations in each group.
+# 4) The dependent variable must follow a normal distribution
+# 5) Assumption of homogeneity of variance
+# Also use when population std is unknown
+# AND when comparing the means of 2 independent samples with equal or unequal variances
 
 # ANOVA (Analysis of Variance)
-# 1) used if dealing with more than 2 groups - comparing means of more than 2 groups
-# 2) for this, we use F-distribution to compare the mean values for more than 2 groups
-# 3) null = samples in all groups are drawn from the same populations with the same mean values
-# 4) reject null = if p-value or sig for f-test is less than 0.05
+# Used if dealing with more than 2 groups - comparing means of more than 2 groups
+# Can't work with continous variable, which is why we need to create categories
+# e.g. Does beauty score for instructors differ by age (instead of 50,51,52 years, we can create 3 age categories, 51-60, 61-70, 71-80)
+# for this, we use F-distribution to compare the mean values for more than 2 groups
+# 1) null = samples in all groups are drawn from the same populations with the same mean values
+# 2) reject null = if p-value or sig for f-test is less than 0.05
 
-# Chi-square test for Association
+
+# Chi-square test for Association (association is the key word, if you hear it, it's chi square)
 # e.g. testing for relationships between categorical variables
 # 1) null: no association between x and y
+
+# Levene's test for equality of variance
+# e.g. important in T-test or ANOVA as it tests the assumption of homogeneity of variance
+# If less than 0.05 then the two variances are significantly different
+# If more than 0.05 then the two variances are statistically equal
+
+# Pearson correlation test
+# Use for continous variables, like age, beauty score, evaluation score etc
