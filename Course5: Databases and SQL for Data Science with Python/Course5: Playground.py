ee39c276f997b4b49030c5e409f3b0220306a8b1
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-06-24 05:56:56 +0100

    Update Course5: Playground.py

--- Course5: Databases and SQL for Data Science with Python/Course5: Playground.py
+++ Course5: Databases and SQL for Data Science with Python/Course5: Playground.py
@@ -1,1 +1,6 @@
+# SQL is a language used for relational databases to get data out of a database
+# What data? Facts (words, numbers), Pictures, Assets
+# What is a database? Repository of data, provides functionality for adding, modyfing and querying that data
+# Basic SQL commands: Create a table, insert, select and update
 
+# Select: 
