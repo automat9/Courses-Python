ef2dd99ffff12f91ad5d34f383622b83b292bcfb
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-07-10 06:39:34 +0100

    Update Course5: Playground.py

--- Course5: Databases and SQL for Data Science with Python/Course5: Playground.py
+++ Course5: Databases and SQL for Data Science with Python/Course5: Playground.py
@@ -563,3 +563,11 @@ LIMIT 5
 
 # Q12: Get the hardship index for the community area which has the highest value for College Enrollment
 # A: N/A as for this solution to work, another table from last week should already exist
+
+# Last bit of info:
+You are provided with a python statement “df.to_sql(‘Sample’, conn)”. What do ‘df’, ‘Sample’ and ‘conn’ refer to?
+df – data frame; Sample – table name ; conn – connection variable
+
+
+
+
